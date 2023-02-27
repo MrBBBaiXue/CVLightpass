a3665b08ef2d423ff7d76937f162a76a38f0acc9
MrBBBaiXue <mrbbbaixue@bakneko.com>
2023-02-27 20:18:10 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CVLightpass
-A simple OpenCVSharp cross-platform implementation of image enhancement algorithm (Retinex).
+# CVLightPass
+A simple implementation of image enhancement algorithm (Retinex).
---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+import tkinter
+import cv2
+
+def main():
+    testimg = cv2.imread('./data/testimg.jpg')
+    cv2.imshow('testimage', testimg)
+    cv2.waitKey(0)
+    pass
+
+
+if __name__ == '__main__':
+    # call main function
+    main()
