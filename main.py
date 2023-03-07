e73eb9b95b93cf7f076eb5a4a0c5f58087771ea9
MrBBBaiXue <mrbbbaixue@bakneko.com>
2023-03-08 00:54:09 +0800

    Basic Frame

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import numpy as np
-import tkinter
+import tkinter as tk
+import customtkinter as ctk
 import cv2
 
+# Init tkinter
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+window = ctk.CTk()
+
+
 def main():
-    testimg = cv2.imread('./data/testimg.jpg')
-    cv2.imshow('testimage', testimg)
-    cv2.waitKey(0)
+    window.title('CVLightPass')
+    window.geometry('1280x660')
+    # Create image stack panel
+    src_frame = ctk.CTkFrame(master=window, width=480, height=640)
+    dst_frame = ctk.CTkFrame(master=window, width=480, height=640)
+    src_frame.pack(side='left', padx=10, pady=10)
+    dst_frame.pack(side='left', padx=0, pady=10)
+    
+    #testimg = cv2.imread('./data/testimg.jpg')
+    window.mainloop()
+    #cv2.imshow('testimage', testimg)
+    #cv2.waitKey(0)
     pass
 
 
